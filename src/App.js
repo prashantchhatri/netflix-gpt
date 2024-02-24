28620c3c3100d3817033131d54e949a15854e971
prashantchhatri <prashantchhatri2022@gmail.com>
2024-02-24 10:12:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div className="text-3xl text-green-800">
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./Header";
+import Login from "./Login";
+
+const Body = () => {
+  return (
+    <div>
+      <Header/>
+      <Login/>
+    </div>
+  )
+}
+
+export default Body
